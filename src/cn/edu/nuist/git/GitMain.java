9dc7567d067df92826499ebafc3d11dd6a1d8b11
lofly <2337343427@qq.com>
2020-12-24 17:06:23 +0800

    fir commit

---
+++ src/cn/edu/nuist/git/GitMain.java
@@ -0,0 +1,10 @@
+package cn.edu.nuist.git;
+
+public class GitMain {
+
+	public static void main(String[] args) {
+		System.out.println("Git");
+		Happy h = new Happy();
+		h.init();
+	}
+}
---
+++ src/cn/edu/nuist/git/Happy.java
@@ -0,0 +1,9 @@
+package cn.edu.nuist.git;
+
+public class Happy {
+    
+     public  void init() {
+    	 System.out.println("初始化方法1");
+    	 System.out.println("测试1");
+     }
+}
